211e53d2adb70153980e7aaad048264817144a09
Kashika08 <kmalick@vclvm178-98.vcl.ncsu.edu>
2025-03-18 23:11:52 -0400

    Fetching metrics

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Compiling fetch_metrics.cpp..."
-g++ -o fetch_metrics fetch_metrics.cpp -lcurl
-echo "Build complete. Run ./fetch_metrics to fetch Kubernetes metrics!"
-
--- port_forward.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Forwarding Kubernetes Metrics Server API to localhost:8443..."
-kubectl port-forward -n kube-system service/metrics-server 8443:https
-
